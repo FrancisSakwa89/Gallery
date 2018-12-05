2f442bcefdb895efe31fa676df61e20501a53e3e
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-12-05 21:47:34 +0300

    added class location in models.py file

--- photos/models.py
+++ photos/models.py
@@ -5,3 +5,8 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+class Location(models.Models):
+    location = models.CharField(max_length = 60)    
+
+    def __str__(self):
+        return self.location
