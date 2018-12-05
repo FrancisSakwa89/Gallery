aa6f0fd2dc498f892395e49f53849ddb924f7ad2
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-12-05 22:32:58 +0300

    added a category class in models.py file file

--- photos/models.py
+++ photos/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
 
 class Location(models.Model):
     location = models.CharField(max_length = 60)    
@@ -13,3 +9,16 @@ class Location(models.Model):
         return self.location
     class Meta:
         ordering = ['location']
+
+# class Image(models.Model):
+#     image = models.ImageField(upload_to = 'photos/')
+#     name = models.CharField(max_length=60)
+#     description = models.TextField()
+#     location = models.ForeignKey(Location,)
+
+class Category(models.Model):
+    category = models.CharField(max_length=60)
+    def __str__(self):
+        return self.category
+    class Meta:
+        ordering = ['category']    
